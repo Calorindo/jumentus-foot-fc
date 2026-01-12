a4b056064898a7656bd4c5e3dbc7fc3bd2b5e0fa
Gabriel Calorindo <gabriel@onhere.com.br>
2026-01-12 11:10:54 -0300

    fix: use effect

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { useFirebaseData } from '@/hooks/useFirebase';
 import { useAuth } from '@/contexts/AuthContext';
 import { createPlayer, updatePlayerStats, deletePlayer as deletePlayerFromDB, permanentlyDeletePlayer, incrementPlayerGoal } from '@/services/addPlayer';
